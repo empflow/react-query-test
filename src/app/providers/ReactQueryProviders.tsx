89bc8cf1c9c36f12b87ae1b1f3f205b53986b1d6
Matvey <empflow@tutanota.com>
2023-08-22 17:43:43 +0300

    feat: add react query provider & devtools to layout

--- src/app/providers/ReactQueryProviders.tsx
+++ src/app/providers/ReactQueryProviders.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactNode, useState } from "react";
 
 export default function ReactQueryProviders({
@@ -10,7 +11,14 @@ export default function ReactQueryProviders({
 }) {
   const [queryClient] = useState(() => new QueryClient());
 
+  const DevTools =
+    process.env.NODE_ENV === "development" ? <ReactQueryDevtools /> : <></>;
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        {DevTools}
+        {children}
+      </QueryClientProvider>
+    </>
   );
 }
