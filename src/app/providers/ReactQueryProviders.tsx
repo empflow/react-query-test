a119018dc112158dff4fa084eec58cabee2ef091
Matvey <empflow@tutanota.com>
2023-08-22 18:50:20 +0300

    refactor: refactor RQ devtools appearance logic

--- src/app/providers/ReactQueryProviders.tsx
+++ src/app/providers/ReactQueryProviders.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import isDev from "@/utils/isDev";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactNode, useState } from "react";
@@ -11,12 +12,10 @@ export default function ReactQueryProviders({
 }) {
   const [queryClient] = useState(() => new QueryClient());
 
-  const DevTools =
-    process.env.NODE_ENV === "development" ? <ReactQueryDevtools /> : <></>;
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        {DevTools}
+        {isDev() && <ReactQueryDevtools />}
         {children}
       </QueryClientProvider>
     </>
