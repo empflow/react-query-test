27b4e5902abbbf09fbd9bc84d0a6bb31995739b7
Matvey <empflow@tutanota.com>
2023-08-23 16:34:15 +0300

    feat: add TQueryErrCodes & TAxiosErrWithResp types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,16 @@
+import { AxiosError, AxiosResponse } from "axios";
+
 export interface TPost {
   title: string;
   body: string;
   userId: number;
   id: number;
 }
+
+export const enum TQueryErrCodes {
+  POSTS_FETCH_FAILED,
+}
+
+export type TAxiosErrWithResp<T = any> = Omit<AxiosError<T>, "response"> & {
+  response: AxiosResponse<T>;
+};
