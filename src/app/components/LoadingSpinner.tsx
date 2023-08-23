2ae7181a9fde821f1e9fef401e774f0c8eb0f626
Matvey <empflow@tutanota.com>
2023-08-23 17:43:53 +0300

    feat: improve loading spinner

--- src/app/components/LoadingSpinner.tsx
+++ src/app/components/LoadingSpinner.tsx
@@ -1,9 +1,22 @@
-import loadingSpinner from "@assets/loadingSpinner.gif";
+import { CSSProperties } from "react";
 
-export default function LoadingSpinner() {
+interface Props {
+  parentStyle?: CSSProperties;
+  childStyle?: CSSProperties;
+  pxSize?: number;
+}
+export default function LoadingSpinner(props: Props) {
+  const { pxSize = 25, childStyle, parentStyle } = props;
+
   return (
-    <div>
-      <img src={loadingSpinner.src} alt="Loading..." />
+    <div
+      className={`relative flex justify-center items-center`}
+      style={{ ...parentStyle, width: pxSize, height: pxSize }}
+    >
+      <div
+        style={childStyle}
+        className={`absolute top-0 bottom-0 left-0 right-0 border-4 border-t-blue-600  border-gray-300 animate-spin rounded-full`}
+      ></div>
     </div>
   );
 }
