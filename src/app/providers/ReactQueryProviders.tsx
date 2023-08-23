33533127d1764843b7542f571dbd5c63ca474394
Matvey <empflow@tutanota.com>
2023-08-23 16:34:45 +0300

    feat: handle RQ errors in ReactQueryProviders.tsx

--- src/app/providers/ReactQueryProviders.tsx
+++ src/app/providers/ReactQueryProviders.tsx
@@ -1,16 +1,31 @@
 "use client";
 
 import isDev from "@/utils/isDev";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import isObject from "@/utils/isObject";
+import queryCacheOnError from "@/utils/queryCacheOnError";
+import { TQueryErrCodes } from "@/utils/types";
+import {
+  Query,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactNode, useState } from "react";
+import { toast } from "react-toastify";
 
 export default function ReactQueryProviders({
   children,
 }: {
   children: ReactNode;
 }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => {
+    return new QueryClient({
+      queryCache: new QueryCache({
+        onError: queryCacheOnError,
+      }),
+    });
+  });
 
   return (
     <>
