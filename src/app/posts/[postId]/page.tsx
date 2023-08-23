87a9dccac53ffbaeb4949451716a98567d29891c
Matvey <empflow@tutanota.com>
2023-08-23 18:24:28 +0300

    feat: add /posts/[postId] page

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+import LoadingSpinner from "@/app/components/LoadingSpinner";
+import usePostQuery from "@/app/hooks/queries/usePostQuery";
+import { useRouter } from "next/navigation";
+import BackBtn from "./components/BackBtn";
+
 interface PostContext {
   params: { postId: string };
 }
 
 export default function Post({ params: { postId } }: PostContext) {
-  return <h1>{postId}</h1>;
+  const router = useRouter();
+  const { data, isError, isLoading } = usePostQuery(+postId);
+
+  if (isError) {
+    return <h1>Something went wrong</h1>;
+  }
+  if (isLoading) return <LoadingSpinner />;
+
+  return (
+    <div>
+      <BackBtn />
+      <p className="font-light">Written by user {data.userId}</p>
+      <h1 className="font-semibold mb-4 text-3xl">{data.title}</h1>
+      <p>{data.body}</p>
+    </div>
+  );
 }
