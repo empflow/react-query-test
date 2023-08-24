74a130cfb07c8944eb62169ed7dee5d66973b291
Matvey <empflow@tutanota.com>
2023-08-24 19:42:12 +0300

    fix: add `key` prop to lines in PostContentSkeleton.tsx

--- src/app/posts/[postId]/components/PostContentSkeleton.tsx
+++ src/app/posts/[postId]/components/PostContentSkeleton.tsx
@@ -4,8 +4,8 @@ export default function PostContentSkeleton() {
   const linesAmount = 4;
   return (
     <div className="flex flex-col gap-1">
-      {fillArr(linesAmount).map((line) => (
-        <p className="bg-gray-200 rounded h-[1rem]"></p>
+      {fillArr(linesAmount).map((item, i) => (
+        <p key={i} className="bg-gray-200 rounded h-[1rem]"></p>
       ))}
     </div>
   );
