1cc9b68224210baf09ce0e50985d027df657c54a
Matvey <empflow@tutanota.com>
2023-08-24 19:41:27 +0300

    feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import NotFoundPageGoBackBtn from "./components/NotFoundPageGoBackBtn";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Page not found</h1>
+      <NotFoundPageGoBackBtn />
+    </div>
+  );
+}
