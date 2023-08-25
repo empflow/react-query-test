f8e4912536859fb3afe1af335981de1db504d272
Matvey <empflow@tutanota.com>
2023-08-25 15:25:07 +0300

    feat: make /users/[userId] SSG

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -1,17 +1,46 @@
-"use client";
-import useChannelQuery from "@/app/hooks/queries/useChannelQuery";
-import useUserQuery from "@/app/hooks/queries/useUserQuery";
+import { baseUrl } from "@/utils/axios";
+import { userSchema } from "@/utils/types";
 
-interface TUserContext {
-  params: { userId: string };
+interface TParams {
+  userId: string;
 }
 
-export default function User({ params: { userId } }: TUserContext) {
-  const userQuery = useUserQuery(userId);
-  const channelId = userQuery.data?.channelId;
-  const channelQuery = useChannelQuery(channelId);
+interface TUserContext {
+  params: TParams;
+}
 
-  console.log(userQuery.data);
-  console.log(channelQuery.data);
-  return <h1></h1>;
+export async function generateStaticParams(): Promise<TParams[]> {
+  const resp = await fetch(`${baseUrl}/users`);
+  const data = await resp.json();
+  const users = userSchema.array().parse(data);
+  return users.map((user) => ({
+    userId: user.id.toString(),
+  }));
+}
+
+export default async function User({ params: { userId } }: TUserContext) {
+  const { name, id, address, company, email, phone, website } = await fetchUser(
+    userId
+  );
+
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold mb-2">{name}</h1>
+
+      <p>ID: {id}</p>
+      <p>
+        Address: {address.city}, {address.street}
+      </p>
+      <p>Company: {company.name}</p>
+      <p>Email: {email}</p>
+      <p>Phone number: {phone}</p>
+      <p>Website: {website}</p>
+    </div>
+  );
+}
+
+async function fetchUser(userId: string) {
+  const resp = await fetch(`${baseUrl}/users/${userId}`);
+  const data = await resp.json();
+  return userSchema.parse(data);
 }
