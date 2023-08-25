86ba5d7b9940eb878c2410eaca0e6ae3a2473403
Matvey <empflow@tutanota.com>
2023-08-25 14:50:34 +0300

    feat: add loading page for /posts/[postId]

---
+++ src/app/posts/[postId]/loading.tsx
@@ -0,0 +1,15 @@
+import LoadingSpinner from "@/app/components/LoadingSpinner";
+import fillArr from "@/utils/fillArr";
+
+export default function Loading() {
+  return (
+    <div className="flex flex-col gap-[2px]">
+      <p className="h-[32px] bg-gray-200 rounded"></p>
+      <p className="h-[40px] bg-gray-200 rounded"></p>
+
+      {fillArr(10).map((line, i) => (
+        <p className="h-[0.95rem] bg-gray-200 rounded"></p>
+      ))}
+    </div>
+  );
+}
