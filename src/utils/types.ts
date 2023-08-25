ffdbe24a618b88162c3a39644880de10ba11c74a
Matvey <empflow@tutanota.com>
2023-08-25 15:24:00 +0300

    feat: update user schema

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -21,9 +21,24 @@ export const postSchema = z.object({
 export type TPost = z.infer<typeof postSchema>;
 
 export const userSchema = z.object({
-  name: z.string(),
-  channelId: z.number(),
   id: z.number(),
+  name: z.string(),
+  username: z.string(),
+  email: z.string().email(),
+  address: z.object({
+    street: z.string(),
+    suite: z.string(),
+    city: z.string(),
+    zipcode: z.string(),
+    geo: z.object({ lat: z.string(), lng: z.string() }),
+  }),
+  phone: z.string(),
+  website: z.string(),
+  company: z.object({
+    name: z.string(),
+    catchPhrase: z.string(),
+    bs: z.string(),
+  }),
 });
 
 export type TUser = z.infer<typeof userSchema>;
