e30ccb53c27fa05dfd0d2d00e8d7f368f0d849a7
Matvey <empflow@tutanota.com>
2023-08-26 17:32:34 +0300

    feat: update ["posts"] query after successfully creating a post

--- src/app/hooks/queries/useAddPostMutation.ts
+++ src/app/hooks/queries/useAddPostMutation.ts
@@ -1,13 +1,23 @@
-import { TPost } from "@/utils/types";
-import { useMutation } from "@tanstack/react-query";
+import { TPost, postSchema } from "@/utils/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "@/utils/axios";
 
 export default function useAddPostMutation() {
-  async function addPost(post: Omit<TPost, "id">) {
-    const { origin } = location;
-    const { data } = await axios.post(`${origin}/api/add-post`, post);
-    return data;
-  }
+  const queryClient = useQueryClient();
 
-  return useMutation(addPost, { mutationKey: ["posts"] });
+  return useMutation(addPost, {
+    onSuccess: (data) => {
+      const post = postSchema.parse(data);
+      queryClient.setQueryData(["posts"], (prevData) => {
+        const prevPosts = postSchema.array().parse(prevData);
+        return [...prevPosts, post];
+      });
+    },
+  });
+}
+
+async function addPost(post: Omit<TPost, "id">) {
+  const { origin } = location;
+  const { data } = await axios.post(`${origin}/api/add-post`, post);
+  return data;
 }
