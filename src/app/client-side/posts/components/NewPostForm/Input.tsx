a82c6a1036bb9cf982874c446687d95c5cbb937c
Matvey <empflow@tutanota.com>
2023-08-26 17:04:35 +0300

    feat: improve <Input /> input component
    What's added:
    1. ability to add any attribute of a normal <input /> jsx element
    2. styling
    3. ability to pass the label text as a prop
    4. ability to pass an error message as a prop

--- src/app/client-side/posts/components/NewPostForm/Input.tsx
+++ src/app/client-side/posts/components/NewPostForm/Input.tsx
@@ -1,16 +1,32 @@
 import { UseFormRegisterReturn } from "react-hook-form";
+import Label from "./Label";
+import { InputHTMLAttributes } from "react";
+import FieldErr from "./FieldErr";
 
-interface TInputProps {
+interface TInputProps extends InputHTMLAttributes<HTMLInputElement> {
   type: string;
   register: UseFormRegisterReturn<string>;
+  label: string;
+  errMsg?: string;
 }
 
-export default function Input({ register, type }: TInputProps) {
+export default function Input({
+  register,
+  type,
+  label,
+  errMsg,
+  ...attributes
+}: TInputProps) {
   return (
-    <input
-      type={type}
-      {...register}
-      className="border p-1 border-gray-200 rounded"
-    />
+    <Label>
+      {label}
+      <input
+        type={type}
+        {...register}
+        {...attributes}
+        className="border py-1 px-2 border-gray-300 rounded"
+      />
+      <FieldErr content={errMsg} />
+    </Label>
   );
 }
